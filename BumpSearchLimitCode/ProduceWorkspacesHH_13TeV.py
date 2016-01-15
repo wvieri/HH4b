3d481544e4fccf5073fe1da28b8f620e23d35e72
maxime <mgouzevi@cern.ch>
2016-01-15 13:27:11 +0100

    Updated

--- BumpSearchLimitCode/ProduceWorkspacesHH_13TeV.py
+++ BumpSearchLimitCode/ProduceWorkspacesHH_13TeV.py
@@ -10,6 +10,8 @@ for mass in masses:
   outputfile.write('#!/bin/bash\n')
   outputfile.write("cd ${CMSSW_BASE}/src/HH4b/BumpSearchLimitCode; eval `scramv1 run -sh`\n")
   outputfile.write("root -b -q 'R2JJFitterHH_13TeV.cc("+str(mass)+","+'""'+",0)'\n")
+  outputfile.write("combineCards.py datacards/CMS_jj_HH_"+str(mass)+"_13TeV_CMS_jj_3btag_cat1.txt datacards/CMS_jj_HH_"+str(mass)+"_13TeV_CMS_jj_4btag_cat0.txt &>datacards/CMS_jj_HH_"+str(mass)+"_13TeV_CMS_jj_2cat.txt")
+
   outputfile.close()
   
   command="rm "+logname
