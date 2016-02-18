ae52a6f38e7520507c170a59e87d47b8c0590868
maxime <mgouzevi@cern.ch>
2016-02-18 18:59:45 +0100

    Updated with Integer fix

--- BumpSearchLimitCode/MiniTreeProducerHH_13TeV.C
+++ BumpSearchLimitCode/MiniTreeProducerHH_13TeV.C
@@ -8,12 +8,14 @@
  normWeight = 1;
 
 
- string sInFile = "input/MassPlotFineBins_btagsf.root";
+ string sInFile = "input/MassPlotFineBins_Moriond.root";
+ // string sInFile = "input/MassPlotFineBins_subtr_Moriond.root";
  cout << sInFile.c_str() << endl;
  TFile file0(sInFile.c_str(), "read");
  
 
  string sOutFile("MiniTrees/Background_HH_13TeV/dijetHH_miniTree.root");
+ // string sOutFile("MiniTrees/Background_HH_13TeV/dijetHH_subtr_miniTree.root");
  TFile f1(sOutFile.c_str(), "recreate");
  f1.cd();
  
@@ -40,10 +42,10 @@
       
      mgg = Axis->GetBinCenter(i);
      
-     normWeight = N;
+     normWeight = TMath::Nint(N)*1.0;
      categories = iCat;
  
-     if (i%10 == 0) cout << "i = " << i << " N = " << N << " binCenter = " << hMass->GetBinCenter(i)<< " categories = " << categories << endl;
+     if (N > 1e-10) cout << "i = " << i << " N = " << N << " normWeight = " << normWeight << " binCenter = " << hMass->GetBinCenter(i)<< " categories = " << categories << endl;
 
     if (N > 1e-10 && mgg > 1000 && mgg < 5000) TCVARS->Fill();
    }
